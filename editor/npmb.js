0d52d9ec0e9d1365c84d6f9729dc6e78e8f870dc
Gamermertcan <115120358+Gamermertcan@users.noreply.github.com>
2024-11-08 21:51:00 +0300

    Update npmb.js

--- editor/npmb.js
+++ editor/npmb.js
@@ -12,7 +12,7 @@ Blockly.Blocks['npm_install'] = {
 };
 
 javascript.javascriptGenerator.forBlock['npm_install'] = function(block) {
-  const _package = javascript.valueToCode(block, 'package', javascript.Order.ATOMIC);
+  const _package = javascript.valueToCode(block, 'npm install [package]', javascript.Order.ATOMIC);
   const code = `require('child_process').exec('npm install ${_package}', (err, stdout, stderr) => { 
     if (err) console.error('Error:', err); 
     else {
@@ -27,7 +27,7 @@ Blockly.Blocks['npm_raw'] = {
   init: function() {
     this.appendValueInput("command")
         .setCheck("String")
-        .appendField("npm");
+        .appendField("command");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -37,7 +37,7 @@ Blockly.Blocks['npm_raw'] = {
 };
 
 javascript.javascriptGenerator.forBlock['npm_raw'] = function(block) {
-  const _command = javascript.valueToCode(block, 'command', javascript.Order.ATOMIC);
+  const _command = javascript.valueToCode(block, 'npm [command]', javascript.Order.ATOMIC);
   const code = `require('child_process').exec('npm ${_command}', (err, stdout, stderr) => { 
     if (err) console.error('Error:', err); 
     else {
